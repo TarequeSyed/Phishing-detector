9e2769cf54872732dcc98054bf59acc74118cf9a
TarequeSyed <tareque1234syed@gmail.com>
2026-03-09 00:07:16 +0530

    Added the function of heuristic_score and tested it

---
+++ backend/analyser/heuristic.py
@@ -0,0 +1,36 @@
+"""
+This is Rule-based detection. It means even if Ai fails, the system
+will still detect basic phishing signals.
+"""
+
+
+def heuristic_score(email_text):
+    """
+    Calculates phishing risks using the score based on suscipious keywords
+    """
+
+    suspicious_keywords = [
+         "urgent",
+        "verify your account",
+        "password",
+        "bank",
+        "login immediately",
+        "click here",
+        "suspended",
+        "security alert",
+        "confirm your identity",
+        "limited time"
+    ]
+
+    score = 0
+    detected_keywords = []
+
+    text = email_text.lower()
+
+    for keywords in suspicious_keywords:
+        if keywords in text:
+            score += 10
+            detected_keywords.append(keywords)
+    
+    return score, detected_keywords
+
--- backend/test.py
+++ backend/test.py
@@ -1,4 +1,5 @@
 from analyser.url_detector import extract_urls
+from analyser.heuristic import heuristic_score
 
 email = """
     please verify your account immediately:
@@ -6,3 +7,17 @@ email = """
     """
 
 print(extract_urls(email))
+
+email = """
+    Dear customer, 
+
+    your bank account has been suspended.
+    Please verify your account immediately.
+    Click here to login.
+    
+    """
+
+score, keywords = heuristic_score(email)
+
+print("Score: ", score)
+print("keywords: ", keywords)
